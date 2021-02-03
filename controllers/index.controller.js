61f7c9796e4f79d4e33ff353695ff9838cd98a59
tjonty <tejanijonty2323@gmail.com>
2021-02-03 17:45:57 +0530

    complete

--- controllers/index.controller.js
+++ controllers/index.controller.js
@@ -1,44 +1,70 @@
 //import model
 const userM = require("../models/user.model");
 
-
-let getM = (req, res) => {
-    console.log("in get method");
-    res.send("in get method.");
-}
-
 let create = (req, res) => {
-    console.log("in post method");
-    console.log(req.body);
-    let newUser = new userM(req.body);
-    newUser.save((err, doc) => {
-        if (err) return console.log(err);
-        res.send("Document inserted successfully." + doc);
-    });
+    try {
+        if (!req.body) {
+            throw new Error("body missing!");
+        }
+        let newUser = new userM(req.body);
+        newUser.save((err, doc) => {
+            if (err) return res.send({ success: false, msg: err.message });
+            res.send("Document inserted successfully." + doc);
+        });
+    }
+    catch (err) {
+        res.send({ success: false, message: err.message });   
+    }  
 }
 
 let read = async (req, res) => {
-    console.log("in read");
-    console.log(req.body);
-    await userM.find({}, (err, docs) => {
-        if (err) res.send({ success: false, msg: "failed to retrive data" });
-        res.send(docs);
-    });    
+    try {
+        await userM.find({}, (err, docs) => {
+            if (err) return res.send({ success: false, msg: err.message });
+            res.send(docs);
+        }); 
+    }   
+    catch (err) {
+        res.send({ success: false, message: err.message });
+    }   
 }
 
 let update = async (req, res) => {
-    console.log("in update");
-    console.log(req.body);
-    await userM.updateOne({ email: req.body.email }, { $set: { name: req.body.updatename } }, (err, docs) => {
-        if (err) res.send("failed to update");
-        res.send("successfully update.");
-    });
+    try {
+        if (!req.body.email || !req.body.updatename) {
+            throw new Error("body or updatename missing!");
+        }
+        await userM.updateOne({ email: req.body.email }, { $set: { name: req.body.updatename } }, (err, docs) => {
+            if (err) return res.send({ success: false, msg: err.message });
+            res.send(docs);
+        });
+    }
+    catch (err) {
+        res.send({ success: false, message: err.message });
+    }
 } 
+
+let deleteD = async (req, res) => {
+    try { 
+        if (!req.body.email) {
+            throw new Error("Email missing!");
+        }
+        userM.deleteOne({ email: req.body.email }, (err, docs) => {
+            console.log("err:"+ err + " docs: " + docs.deletedCount);
+            if (err) return res.send({ success: false, msg: err.message });
+            if (docs.deletedCount == 0) return res.send("successfully delete one.");
+            res.send("failed to delete.");
+        });   
+    }
+    catch (err) {
+        res.send({ success: false, message: err.message });        
+    }
+}
 
 //export modules
 module.exports = {
-    getM,
     create,
     read,
-    update
+    update,
+    deleteD
 }
--- routes/index.routes.js
+++ routes/index.routes.js
@@ -5,10 +5,10 @@ const indexR = express.Router();
 const indexC = require("../controllers/index.controller");
 
 //apis
-indexR.get("/", indexC.getM);
 indexR.post("/C", indexC.create);
 indexR.get("/R", indexC.read);
 indexR.put("/U", indexC.update);
+indexR.delete("/D", indexC.deleteD);
 
 //export modules
 module.exports = indexR;
